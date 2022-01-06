cee3cc71d43a590d17de6e4d2ff2addf73042ab3
Chrys <89508217+chry8822@users.noreply.github.com>
2022-01-07 01:03:59 +0900

    support 페이지, about 페이지 수정
    support 페이지 미디어 쿼리 추가, 스타일 수정
    about 페이지 비디오 태그 수정, 미디어 쿼리 수정(백그라운드 이미지 교체 , 스타일 수정 등)
    공통 히든 메뉴 스타일 수정

--- JS/about.js
+++ JS/about.js
@@ -16,7 +16,7 @@ function aboutclick() {
   // };
 }
 function windowabout(event) {
-  console.log('클릭')
+  console.log('클릭', event.path,event.composedPath)
   if (event.path.includes(show) && !event.path.includes(aboutlist)) {
     aboutclose();
     about.style.display = 'flex';    
@@ -27,44 +27,9 @@ function aboutclose () {
   show.style.opacity = 0;
 }
 
-//  메뉴 열기
-function menuOpen(e) {
-  e.stopPropagation();
-  hidemenu.style.transform = "translate(" + 0 + "%)";
-  // window.onclick = function (event) {
-  //   if (!event.path.includes(hidemenu)) {
-  //     menuClose();
-  //     window.onclick = null;
-  //   }
-  // };
-}
-function windowclose (event) {
-  if (!event.path.includes(hidemenu)) {
-    menuClose();
-    window.onclick = null;
-    window.ontouchstart = null;
-  }
-};
-// 메뉴 닫기
-function menuClose() {
-  hidemenu.style.transform = "translate(" + 100 + "%)";
-}
-// 화면 위로 올리는 버튼
-function up() {
-  window.scrollTo({ top: loca2, behavior:"smooth"});
-}
-
 window.addEventListener('click', windowabout)
 window.addEventListener('touchstart', windowabout)
 
 about.addEventListener("touchstart", aboutclick)
 about.addEventListener('click', aboutclick)
 
-menu.addEventListener("click", menuOpen);
-menu.addEventListener("touchstart", menuOpen);
-
-closemenu.addEventListener("click", menuClose);
-closemenu.addEventListener("touchstart", menuClose);
-
-window.addEventListener('touchstart', windowclose);
-window.addEventListener('click', windowclose);
